efb79054c5f1df4d8508ee4a77ce281844f643f8
Roman Artemyev <artemyev.ram@gmail.com>
2023-03-03 18:30:15 +0300

    Шаг 25 рефакторинг кода с использованием интервалов (in 0 .. 30)

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,33 +1,35 @@
 
 fun main() {
     val name = "ТриДогНайт"
-    var healthPoints = 87
+    var healthPoints = 84
     val regeneration = true
     val isImmortal = false
 
     // Аура удачи
     val luck = regeneration && healthPoints > 50 || isImmortal //Присваивание переменной значения типа boolean
     val auraLuck = if (luck) "Сопутствует удача" else "Удача отвернулась"
+    println(auraLuck)
 
     val healthStatus = if (healthPoints == 100) {
         "в отличном состоянии."
-    }else if(healthPoints >= 85) {
+    }else if(healthPoints in 85..99) {
         if(regeneration)
             "включил режим ускоренной регенерации"
             else
         "в хорошем состоянии."
-    }else if(healthPoints >= 60){
+    }else if(healthPoints in 45..84){
         "ранен"
-    }else if(healthPoints >= 45){
+    }else if(healthPoints in 30..44){
         "сильно ранен"
-    } else if (healthPoints >= 30)
+    } else if (healthPoints in 16..29)
         "еле стоит на ногах"
-    else if (healthPoints >= 15) {
+    else if (healthPoints in 0..15) {
         "вот-вот потеряет сознание"
     }else
         "Приплыли"
 
     // Состояние игрока
     println(name + " " + healthStatus)
+
 
 }
