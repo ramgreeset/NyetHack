cef0a4a419384c900be35356b9599bcbe2fe9b7c
Roman Artemyev <artemyev.ram@gmail.com>
2023-03-03 17:53:05 +0300

    Шаг 24. Оптимизация читаемости кода, за счет присваевания результата выражений if в переменные и исключения повторяющихся участков с вызовом функции println на каждой ветке исполнения программы

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,35 +1,33 @@
 
 fun main() {
     val name = "ТриДогНайт"
-    var healthPoints = 60
+    var healthPoints = 87
     val regeneration = true
     val isImmortal = false
 
-    // Аура
-    val auraVisible = regeneration && healthPoints > 50 || isImmortal //Присваивание переменной значения типа boolean
-    if (auraVisible) {
-        println("Аура")
-    } else {
-        println("NONE")
-    }
+    // Аура удачи
+    val luck = regeneration && healthPoints > 50 || isImmortal //Присваивание переменной значения типа boolean
+    val auraLuck = if (luck) "Сопутствует удача" else "Удача отвернулась"
 
-    if (healthPoints == 100) {
-        println(name + " в отличном состоянии.")
+    val healthStatus = if (healthPoints == 100) {
+        "в отличном состоянии."
     }else if(healthPoints >= 85) {
         if(regeneration)
-            println(name + " включил режим ускоренной регенерации")
+            "включил режим ускоренной регенерации"
             else
-        println(name + " в хорошем состоянии.")
+        "в хорошем состоянии."
     }else if(healthPoints >= 60){
-        println(name + " ранен")
+        "ранен"
     }else if(healthPoints >= 45){
-        println(name + " сильно ранен")
+        "сильно ранен"
     } else if (healthPoints >= 30)
-        println(name + " еле стоит на ногах")
+        "еле стоит на ногах"
     else if (healthPoints >= 15) {
-        println(name + " вот-вот потеряет сознание")
+        "вот-вот потеряет сознание"
     }else
-        println("Приплыли")
+        "Приплыли"
 
+    // Состояние игрока
+    println(name + " " + healthStatus)
 
 }
