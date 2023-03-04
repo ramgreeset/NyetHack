4e59db32ca90f9ebfef55f6f4ef600e8e96f11cf
Roman Artemyev <artemyev.ram@gmail.com>
2023-03-04 12:48:12 +0300

    Шаг 29 Домашнее задание

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,23 @@
 
 fun main() {
-    val name = "ТриДогНайт"
-    var healthPoints = 51
+    val name = "Радмир"
+    var healthPoints = 5
     val isBlessed = true
     val isImmortal = false
 
 
     // Аура
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal //Присваивание переменной значения типа boolean
-    val auraColor = if (auraVisible) "Зеленая" else "Нет ауры"
+//    val auraColor = if (auraVisible) "Зеленая" else "Нет ауры"
+    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0)  * 20).toInt()
+
+    val auraColor = when (karma) {
+        in 0..5 -> "Красный"
+        in 6..10 -> "Оранжевый"
+        in 11..15 -> "Пурпурный"
+        in 16..20 -> "Зеленый"
+        else -> "Нет"
+    }
 
     val healthStatus = when (healthPoints) {
         100 -> "в отличном состоянии."
@@ -16,9 +25,9 @@ fun main() {
         in 45..84 -> "ранен"
         in 30..44 ->"сильно ранен"
         in 16..29 ->"еле стоит на ногах"
-        in 0..15 ->"вот-вот потеряет сознание"
+        in 1..15 ->"вот-вот потеряет сознание"
         else ->
-        "Приплыли"
+        "погиб"
     }
     // Состояние игрока
     println("(Аура: $auraColor)" + "(Благословение: ${if(isBlessed)"Да" else "Нет"})")
