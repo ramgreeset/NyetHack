e795f7b2a43a17a5be8e2ce7f5d01716cd2d78ed
Roman Artemyev <artemyev.ram@gmail.com>
2023-03-04 16:01:34 +0300

    Шаг 35 Рефакторинг кода. Инкапсуляция логики в функции при помощи IntelliJ

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,30 +1,39 @@
 
 fun main() {
     val name = "Радмир"
-    var healthPoints = 5
+    var healthPoints = 15
     val isBlessed = true
     val isImmortal = false
 
 
-
     // Аура
-    val auraVisible = isBlessed && healthPoints > 50 || isImmortal //Присваивание переменной значения типа boolean
-    val auraColor = if (auraVisible) "Зеленая" else "Нет ауры"
-
-
-    val healthStatus = healthStatus(healthPoints, isBlessed)
-
-    // Состояние игрока
-    println("(Аура: $auraColor)" + "(Благословение: ${if(isBlessed)"Да" else "Нет"})")
-    println("$name $healthStatus")
-
-
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+    //Состояние здоровья
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    //Отображение сосстояния игрока
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
 }
 
-private fun healthStatus(healthPoints: Int, isBlessed: Boolean): String {
+private fun printPlayerStatus(
+    auraColor: String,
+    isBlessed: Boolean,
+    name: String,
+    healthStatus: String
+) {
+    println("(Аура: $auraColor)" + "(Благословение: ${if (isBlessed) "Да" else "Нет"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal //Присваивание переменной значения типа boolean
+    val auraColor = if (auraVisible) "Зеленая" else "Нет ауры"
+    return auraColor
+}
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String {
     val healthStatus = when (healthPoints) {
         100 -> "в отличном состоянии."
-        in 85..99 -> if (isBlessed) "включил режим ускоренной регенерации" else "в хорошем состоянии."
+        in 85..99 -> if (isBlessed) "легко ранен, но быстро поправляется" else "в хорошем состоянии."
         in 45..84 -> "ранен"
         in 30..44 -> "сильно ранен"
         in 16..29 -> "еле стоит на ногах"
